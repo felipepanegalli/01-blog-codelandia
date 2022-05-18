c27cba5ebe70610732b09292c593e9d0ff44bf7f
Felipe Panegalli <felipe.panegalli@iffarroupilha.edu.br>
2022-05-18 11:31:13 -0300

    BLOG-01 feat: started blog challenge

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <LayoutTheHeader />
+  <main>
+    <h3>MAIN</h3>
+    <section>POST 01</section>
+    <section>POST 02</section>
+    <section>POST 03</section>
+  </main>
+</template>
---
+++ components/layout/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>Header</header>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 200px;
+  background: var(--gradient-default);
+}
+</style>
