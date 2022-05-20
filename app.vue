ece0207538c2136b4dc7138c810ceeaaa082a195
Felipe Panegalli <felipe.panegalli@iffarroupilha.edu.br>
2022-05-20 08:40:45 -0300

    BLOG-05 feat: change the news and fix css errors

--- app.vue
+++ app.vue
@@ -2,43 +2,34 @@
     <LayoutTheHeader />
     <LayoutTheMain>
         <Card like>
-            <template v-slot:date>
-                02 de jul, 2021
-            </template>
-            <template v-slot:title>Agora é oficial: o Windows 11 está vindo</template>
+            <template v-slot:date>20 de mai, 2021</template>
+            <template v-slot:title>Android 13 beta will test out-of-the-box support for most braille displays</template>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia autem iusto reprehenderit temporibus
-            tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
-            voluptatem hic amet animi?</p>
+                tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
+                voluptatem hic amet animi?</p>
         </Card>
         <Card>
-            <template v-slot:date>
-                02 de jul, 2021
-            </template>
-            <template v-slot:title>Tim Berners-Lee vai leiloar código-fonte da web</template>
+            <template v-slot:date>18 de mai, 2021</template>
+            <template v-slot:title>Steam Deck rivals OneXPlayer, Aya, and GPD are planning their revenge with AMD’s
+                6800U</template>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia autem iusto reprehenderit temporibus
-            tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
-            voluptatem hic amet animi?</p>
+                tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
+                voluptatem hic amet animi?</p>
         </Card>
         <Card>
-            <template v-slot:date>
-                02 de jul, 2021
-            </template>
-            <template v-slot:title>Tem Firefox novo no pedaço e você vai querer migrar</template>
+            <template v-slot:date>18 de mai, 2021</template>
+            <template v-slot:title>How to check which version of Chrome OS you’re running</template>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia autem iusto reprehenderit temporibus
-            tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
-            voluptatem hic amet animi?</p>
+                tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
+                voluptatem hic amet animi?</p>
         </Card>
         <Card like>
-            <template v-slot:date>
-                02 de jul, 2021
-            </template>
-            <template v-slot:title>John MCAfee, criador do antivírus McAfee, morre</template>
+            <template v-slot:date>15 de abr, 2021</template>
+            <template v-slot:title>Now non-Tesla EVs can use its Superchargers in the UK, Spain, Sweden, and
+                Austria</template>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia autem iusto reprehenderit temporibus
-            tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
-            voluptatem hic amet animi?</p>
+                tempore laborum est ratione commodi molestiae? Aliquam excepturi a qui consequuntur accusantium culpa
+                voluptatem hic amet animi?</p>
         </Card>
     </LayoutTheMain>
 </template>
-
-<style lang="scss" scoped>
-</style>
